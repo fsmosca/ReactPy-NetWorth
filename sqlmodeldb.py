f9996d5ca0b46080a321596121f24c2e3d7d2799
fsmosca <fsmosca@users.noreply.github.com>
2023-08-05 15:30:43 +0800

    Create sqlmodeldb.py

---
+++ sqlmodeldb.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class Deal(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    date: str
+    value: float
+    category: str
+    comment: str
+
+
+sqlite_file_name = "networth.db"
+sqlite_url = f"sqlite:///data/{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=False)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def add_deal(date: str, value: float, category: str, comment: str):
+    """Adds an entry to the table."""
+    deal = Deal(
+        date=date,
+        value=value,
+        category=category,
+        comment=comment
+    )
+
+    with Session(engine) as session: 
+        session.add(deal)
+        session.commit()
+
+
+def select_deals():
+    """Gets all reacords from the table."""
+    with Session(engine) as session:
+        statement = select(Deal)
+        results = session.exec(statement)
+        return results.all()
+    
+
+def delete_deal(id_: int):
+    """Deletes an entry from a given id."""
+    with Session(engine) as session:
+        statement = select(Deal).where(Deal.id == id_)
+        results = session.exec(statement)
+        deal = results.one()
+        session.delete(deal)
+        session.commit()
